4b8e89724eea2fbfce4b7f71c54760d480ad205a
David Qu <davidqu12345@gmail.com>
2015-04-14 23:00:52 -0700

    Update config_parser_test.cc

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -10,7 +10,11 @@ TEST(NginxConfigParserTest, SimpleConfig) {
   EXPECT_TRUE(success);
 }
 
-/* New test for FastCGIConfig */
+/* 
+ * New test for FastCGIConfig
+ * The file fastCGI_config is based on examples from
+ * http://nginx.org/en/docs/beginners_guide.html#fastcgi
+ */
 TEST(NginxConfigParserTest, FastCGIConfig) {
   NginxConfigParser parser;
   NginxConfig out_config;
